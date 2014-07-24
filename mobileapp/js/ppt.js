279eb70899429098a51d1d718bbd287edea24d4d
yamironov <yamironov@gmail.com>
2014-07-24 18:18:32 +0400

    Mobile App

--- mobileapp/js/ppt.js
+++ mobileapp/js/ppt.js
@@ -8,6 +8,3 @@ var $$ = Framework7.$;
 
 // Add views
 var view1 = myApp.addView('#view-1');
-var view2 = myApp.addView('#view-2');
-var view3 = myApp.addView('#view-3');
-var view4 = myApp.addView('#view-4');
