1bcff074cedffb1d54df27d73ca9cfff7af54d3e
yamironov <yamironov@gmail.com>
2014-07-25 13:30:41 +0400

    Mobile App

--- mobileapp/js/my-app.js
+++ mobileapp/js/my-app.js
@@ -11,3 +11,5 @@ var view1 = myApp.addView('#view-1');
 var view2 = myApp.addView('#view-2');
 var view3 = myApp.addView('#view-3');
 var view4 = myApp.addView('#view-4');
+
+var historySlider = myApp.slider('.places-history', {pagination: '.slider-pagination', onSlideChangeEnd: function(slider) {myApp.showTab('#place' + slider.activeSlideIndex);}})
